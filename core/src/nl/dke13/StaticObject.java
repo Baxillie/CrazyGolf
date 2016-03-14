1529d17dcee715a0e3b7d27528510d210f00d7e8
baxillie <nikvaes@gmail.com>
2016-03-14 17:22:01 +0100

    static object

---
+++ core/src/nl/dke13/StaticObject.java
@@ -0,0 +1,20 @@
+package nl.dke13;
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Created by nik on 3/14/16.
+ */
+public class StaticObject {
+
+    ModelInstance object;
+    Rectangle rectangle;
+
+    public StaticObject(ModelInstance object, float modelWidth, float modelHeight, float modelX, float modelY) {
+        this.object = object;
+        rectangle = new Rectangle(modelX, modelY, modelWidth, modelHeight);
+    }
+
+
+}
--- core/src/nl/dke13/physics.java
+++ core/src/nl/dke13/physics.java
@@ -1,11 +1,14 @@
 package nl.dke13;
 
+import java.util.ArrayList;
+
 /**
  * Created by nik on 3/14/16.
  */
 public class physics
 {
-
+    ArrayList<DynamicObject> dynamicObjects;
+    ArrayList<StaticObject> staticObjects;
 
 
 
