a1296255f298ebac5767ff2cf44b5f857362bd29
ajki <ajkimino@rocketmail.com>
2016-03-17 20:33:40 +0100

    working menu with music no graphics

--- core/src/nl/dke13/screens/MenuScreen.java
+++ core/src/nl/dke13/screens/MenuScreen.java
@@ -1,14 +1,18 @@
 package nl.dke13.screens;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -16,110 +20,113 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Timer;
 
-public class MenuScreen implements Screen{
-	
-	Skin skin;
-	Stage stage;
-	SpriteBatch batch;
-	
-	Game g;
+public class MenuScreen implements Screen {
+	private SpriteBatch batch;
+	private SpriteBatch batch1;
+	private Texture texture;
+	private Sprite sprite;
+	private Stage stage;
+	private Skin skin;
+	private Game g;
+
 	public MenuScreen(Game g){
-		create();
 		this.g = g;
-	}
-	
-	public MenuScreen(){
 		create();
 	}
+
 	private void create() {
-		
 		batch = new SpriteBatch();
+		texture = new Texture(Gdx.files.internal("core/assets/t_woods.png"));
+		sprite = new Sprite(texture);
+
+		//Button1
+		batch1 = new SpriteBatch();
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
-		
-		Skin skin = new Skin();
-		
-		Pixmap pixmap = new Pixmap(100,100,Format.RGBA8888);
-		pixmap.setColor(0,1,0,0.75f);
-		Texture pixmaptex = new Texture( pixmap );
+
+		skin = new Skin();
+
+		Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
+		pixmap.setColor(Color.GREEN);
 		pixmap.fill();
-		
+
 		skin.add("white", new Texture(pixmap));
-		
+
 		BitmapFont bfont = new BitmapFont();
-		skin.add("default", bfont);
-		
+		//bfont.scale(1);
+		skin.add("default",bfont);
+
 		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		//textButtonStyle.up = skin.newDrawable("white", "core/assets/tiger_woods.png");
-		//textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-		//textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
-		//textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-		
+
 		textButtonStyle.font = skin.getFont("default");
-		
-		skin.add("default",  textButtonStyle);
-		
-		final TextButton textButton = new TextButton("PLAY", textButtonStyle);
+		skin.add("default", textButtonStyle);
+
+		final TextButton textButton=new TextButton("PLAY",textButtonStyle);
 		textButton.setPosition(200, 200);
 		stage.addActor(textButton);
 
-		textButton.addListener(new ChangeListener(){
-
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				System.out.println("hey :))))");
+		textButton.addListener(new ChangeListener() {
+			public void changed (ChangeEvent event, Actor actor) {
+				//System.out.println("Clicked! Is checked: " + button.isChecked());
 				textButton.setText("Starting new game");
 				g.setScreen(new CrazyGolf());
 			}
 		});
-			
-		}
-		
-	public void render(float delta){
-		
-		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+
+		final Sound mp3Sound = Gdx.audio.newSound(Gdx.files.internal("core/assets/tiger_woods_song.mp3"));
+		mp3Sound.play();
+		final long id = mp3Sound.loop();
+		Timer.schedule(new Timer.Task(){
+			@Override
+			public void run(){
+				mp3Sound.stop(id);
+			}
+		}, 3.0f);
+
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+		batch1.dispose();
+	}
+
+	@Override
+	public void render(float delta) {
+		//Background
+		//	Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
 		stage.draw();
-		//Table.drawDebug(stage);
 	}
 
 	@Override
 	public void resize(int width, int height) {
+	}
 
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-	@Override
-	public void dispose() {
-		
-		stage.dispose();
-		skin.dispose();
-		
-	}
-
-
 }
