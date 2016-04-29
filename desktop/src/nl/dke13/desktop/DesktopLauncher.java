e18bc69c64a6dba98611862b9df72cc0d1d2ac2d
PaperResistance <tompetercolm@gmail.com>
2016-04-29 12:56:03 +0200

    new physics

--- desktop/src/nl/dke13/desktop/DesktopLauncher.java
+++ desktop/src/nl/dke13/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import nl.dke13.screens.CrazyGolf;
 import nl.dke13.screens.MainMenu;
 import nl.dke13.screens.MenuScreen;
+import nl.dke13.desktop.HeightmapConverter;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -12,5 +13,6 @@ public class DesktopLauncher {
         config.title = "World of Woods";
 		config.foregroundFPS = 30;
 		new LwjglApplication(new MainMenu(), config);
+		//new LwjglApplication(new HeightmapConverter(200,200,50,"Heightmap.png"), config);
 	}
 }
