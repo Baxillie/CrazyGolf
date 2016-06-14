8abce3173f4f2fee3416413ed6a26fe7eb172f84
baxillie <nikvaes@gmail.com>
2016-06-14 12:02:40 +0200

    non-working translator

--- core/src/nl/dke12/game/GameMap.java
+++ core/src/nl/dke12/game/GameMap.java
@@ -18,6 +18,11 @@ public class GameMap
     private final static int HOLE_RADIUS = 1; //// TODO: 13/06/16 determine actual radius
 
     /**
+     * the amount of cells get created for every unit the game world spans
+     */
+    private final static int UNIT_TO_CELL_RATIO = 4;
+
+    /**
      * stores the spawning location of the golf ball
      */
     private Vector3 startPosition;
@@ -57,7 +62,62 @@ public class GameMap
      */
     private void preMakeGrid()
     {
+        int[][] grid = determineGridDimensions(); //calculates the grid dimensions
+    }
 
+    /**
+     * loops over all objects in the golf course to determine how large the grid has to be
+     */
+    private int[][] determineGridDimensions()
+    {
+        //initialise max and min values
+        SolidObject o = gameObject.get(0); Vector3 pos = o.getPosition();
+
+        //largest  x and y found
+        float maxX = o.getWidth() / 2 - pos.x;
+        float maxY = o.getWidth() / 2 + pos.x;
+        //smallest x and y found
+        float minX = o.getDepth() / 2 - pos.y;
+        float minY = o.getDepth() / 2 + pos.y;
+
+        //loop
+        float width, depth;         //used in loop to store dimensions of every object
+        Vector3 position;           //these dimensions have to be divided by 2 from the center to get max and min
+        float temp;                 //center of object
+        for(SolidObject object : gameObject)
+        {
+            //get data from object
+            width = object.getWidth() / 2;       //x
+            depth = object.getDepth() / 2;       //y
+            position = object.getPosition();
+
+            //determine if this object exceeds the current dimensions
+            if((temp = position.x + width) > maxX)
+            {
+                maxX = temp;
+            }
+            if((temp = position.x - width) < minX)
+            {
+                minX = temp;
+            }
+            if((temp = position.y + depth) > maxY)
+            {
+                maxY = temp;
+            }
+            if((temp = position.y - depth) < minY)
+            {
+                minY = temp;
+            }
+        }
+
+        //calculate actual grid size and return it
+        float absoluteX = Math.abs(maxX - minX);
+        float absoluteY = Math.abs(maxY - minY);
+
+        int gridLength = Math.round(absoluteY);
+        int gridWidth  = Math.round(absoluteX);
+
+        return new int[gridLength * UNIT_TO_CELL_RATIO][gridWidth * UNIT_TO_CELL_RATIO];
     }
 
     /**
